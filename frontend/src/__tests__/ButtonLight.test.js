478d67ef4e3bfe73a749b9cde5f637c5f5065906
mmlait <miia.laitinen@hotmail.com>
2019-07-26 07:31:32 +0300

    add tests

--- frontend/src/__tests__/ButtonLight.test.js
+++ frontend/src/__tests__/ButtonLight.test.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import ButtonLight from '../components/general/ButtonLight';
 import { create } from 'react-test-renderer'
+import { shallow, mount } from 'enzyme';
+import jest from 'jest-mock';
+import ButtonLight from '../components/general/ButtonLight';
+
+let props = {
+  content: 'Ok',
+  type: 'button',
+  onclick: jest.fn()
+}
 
 describe('ButtonLight component', () => {
-    it('should render', () => {
+  it('should render', () => {
     let tree = create(<ButtonLight />)
     expect(tree.toJSON()).toMatchSnapshot();
   })
+  it ('should render with props', () => {
+    const wrapper = mount(
+      <ButtonLight 
+        content={props.content} 
+        type={props.type} 
+        onclick={props.onclick} />
+    );
+    expect(wrapper.props().content).toEqual(props.content);
+    expect(wrapper.props().type).toEqual(props.type);
+    expect(wrapper.props().onclick).toEqual(props.onclick);
+  })
+  it('should call a function when button is clicked', () => {
+    const tree = shallow(
+      <ButtonLight onclick={props.onclick} />
+    );
+    tree.simulate('click');
+    expect(props.onclick).toHaveBeenCalled();
+  });
 })
